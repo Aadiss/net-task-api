6bca96045310b08f8ca52b88c44f88cb537bdf6f
Aadiss <adrian.stadnik00@gmail.com>
2021-12-29 10:54:21 +0100

    final clear

--- NetCars/settings.py
+++ NetCars/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','mqkjqkjn@2190u34128jasknzami21913uijni
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
